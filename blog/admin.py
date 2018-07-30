f5ec09c512e962e8b55c2eb381d31dc8fba8ca83
Jojopanda <ecompx@gmail.com>
2018-07-30 13:31:23 +0300

    Added bootstrap and jobs views are showing up

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,4 @@ from .models import Blog
 
 
 
-
 admin.site.register(Blog)
--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,10 @@ from django.db import models
 
 class Blog(models.Model):
 	title = models.CharField(max_length=40, help_text = 'Enter the title of blog')
-	pub_date = models.DateField(auto_now_add=True, blank=False)
+	pub_date = models.DateField(null=True, blank=True)
 	text = models.TextField(max_length=3000, help_text= "Enter the text")
-	blog_image = models.ImageField(upload_to='images/')
+	blog_image = models.ImageField(upload_to='images/', null=True)
 
+
+	def __str__(self):
+		return '%s' %(self.title)
