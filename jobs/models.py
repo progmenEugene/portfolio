88afdd8e0093111d88820260dc50c2e8da638cbe
Jojopanda <ecompx@gmail.com>
2018-07-30 10:08:01 +0300

    Add jobs model and register this model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+
+#model object for admin site and database
+class Job(models.Model):
+	image = models.ImageField(upload_to = 'images/')
+	summary = models.CharField(max_length=300, help_text = 'Enter your summary')
+
+	
+
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'jobs.apps.JobsConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
