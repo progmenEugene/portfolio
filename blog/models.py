42624ad2eaac25f3d8a99c4ca6a9abc4eae93457
Jojopanda <ecompx@gmail.com>
2018-07-30 10:43:53 +0300

    Add Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#A blog model object for admin site
+
+
+class Blog(models.Model):
+	title = models.CharField(max_length=40, help_text = 'Enter the title of blog')
+	pub_date = models.DateField(auto_now_add=True, blank=False)
+	text = models.TextField(max_length=3000, help_text= "Enter the text")
+	blog_image = models.ImageField(upload_to='images/')
+
