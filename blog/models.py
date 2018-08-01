1051a751e151732fb4485e57827b59065a95da4f
Jojopanda <ecompx@gmail.com>
2018-08-01 17:38:08 +0300

    complete this project(need put photo and resume)

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,13 @@ class Blog(models.Model):
 	title = models.CharField(max_length=40, help_text = 'Enter the title of blog')
 	pub_date = models.DateField(null=True, blank=True)
 	text = models.TextField(max_length=3000, help_text= "Enter the text")
-	blog_image = models.ImageField(upload_to='images/', null=True)
+	blog_image = models.ImageField(upload_to='images/', null=True, blank=True)
 
 
 	def __str__(self):
 		return '%s' %(self.title)
+
+	def summary(self):
+		return self.text[:100]
+
+	
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
@@ -9,6 +9,8 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name="home" ),
+    path('blog/', include('blog.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
